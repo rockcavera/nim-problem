e59de87a8d3539635f95caffa4962fa4bd9e54e7
rockcavera <rockcavera@gmail.com>
2019-11-14 22:14:57 -0300

    Add files via upload

---
+++ codesequ32.nim
@@ -0,0 +1,40 @@
+import strutils
+import streams
+import times
+import algorithm
+
+let
+  fileName1 = "lowu32.txt"
+  fileName2 = "highu32.txt"
+
+proc processFile(fileName: string) =
+  let iTime = epochTime()
+
+  var
+    fh = newFileStream(fileName, fmRead)
+
+  if isNil(fh):
+    raise newException(IOError, "Problem to open file")
+
+  var
+    read: string
+    matriz = newSeqOfCap[uint32](400000)
+    matriz2 = newSeqOfCap[uint32](400000)
+
+  while readLine(fh, read):
+    add(matriz, uint32(parseUInt(read)))
+
+  close(fh)
+
+  sort(matriz)
+
+  for i in matriz:
+    if i != matriz2[^1]: add(matriz2, i)
+
+  let eTime = epochTime()
+  
+  echo fileName, "\nduration: ", eTime - iTime, "\noriginal length: ", len(matriz), "\nfinal length without repeated: ", len(matriz2)
+
+processFile(fileName1)
+echo "\n"
+processFile(fileName2)
---
+++ codesetsu32.nim
@@ -0,0 +1,35 @@
+import strutils
+import streams
+import times
+import math
+import sets
+
+let
+  fileName1 = "lowu32.txt"
+  fileName2 = "highu32.txt"
+
+proc processFile(fileName: string) =
+  let iTime = epochTime()
+
+  var
+    fh = newFileStream(fileName, fmRead, 1000000)
+
+  if isNil(fh):
+    raise newException(IOError, "Problem to open file")
+
+  var
+    read: string
+    matriz = initHashSet[uint32](nextPowerOfTwo(400000))
+
+  while readLine(fh, read):
+    incl(matriz, uint32(parseUInt(read)))
+
+  close(fh)
+
+  let eTime = epochTime()
+  
+  echo fileName, "\nduration: ", eTime - iTime, "\noriginal length: ", 400000, "\nfinal length without repeated: ", len(matriz)
+
+processFile(fileName1)
+echo "\n"
+processFile(fileName2)
